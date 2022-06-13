141dc439e123e4618e12f54c57958bc846163e70
Divya <60733224+divya544@users.noreply.github.com>
2022-06-13 17:06:53 +0530

    Delete scanKaro directory

--- scanKaro/src/main/java/com/scanKaro/repository/GuestRepo.java
+++
@@ -1,9 +0,0 @@
-package com.scanKaro.repository;
-
-import com.scanKaro.dto.Guest;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GuestRepo extends MongoRepository<Guest, String> {
-}
--- scanKaro/src/main/java/com/scanKaro/util/Status.java
+++
@@ -1,9 +0,0 @@
-package com.scanKaro.util;
-
-public enum Status {
-
-        SUCCESS,
-        GUEST_ALREADY_EXISTS,
-        FAILURE
-
-}
